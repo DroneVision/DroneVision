b9cf9cf290b5ec22a80495abc6307d1750de408c
Justin <mintzer.justin@gmail.com>
2019-01-25 14:20:57 -0500

    the threejs canvas is now a child of the #canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -14,35 +14,30 @@ class Canvas extends Component {
     );
 
     this.renderer = new THREE.WebGLRenderer();
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
-
+    this.renderer.setSize(640, 360);
     this.geometry = new THREE.BoxGeometry(1, 1, 1);
     this.material = new THREE.MeshBasicMaterial({ color: 0x488384 });
     this.cube = new THREE.Mesh(this.geometry, this.material);
+    
     this.scene.add(this.cube);
-
     this.camera.position.z = 5;
   }
 
   componentDidMount() {
+    document.getElementById('canvas').appendChild(this.renderer.domElement);
     this.animate();
   }
 
   animate = () => {
     requestAnimationFrame(this.animate);
-
     this.cube.rotation.x += 0.01;
     this.cube.rotation.y += 0.01;
-
     this.renderer.render(this.scene, this.camera);
   };
 
   render() {
     return (
-      <div>
-        <canvas id="canvas" />
+      <div id="canvas">
       </div>
     );
   }
