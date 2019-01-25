b0cf6771e1e2acbd2d4ec6ab6fb514d64fdd3a35
Justin Mintzer <mintzer.justin@gmail.com>
2019-01-25 18:50:27 -0500

    Update Canvas.js

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 
-class Canvas extends Component {
-  constructor(lines) {
-    super();
+const keyboard = {};
 
-    this.state = {
-      sceneItems: [],
-    };
+function keyDown(event) {
+  keyboard[event.keyCode] = true;
+}
+
+function keyUp(event) {
+  keyboard[event.keyCode] = false;
+}
+
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+class Canvas extends Component {
+  constructor() {
+    super();
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
@@ -19,40 +28,63 @@ class Canvas extends Component {
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(640, 360, false);
-
-    function createPlane() {}
     this.planeGeo = new THREE.PlaneBufferGeometry(30, 30, 500, 500);
     this.planeMaterial = new THREE.MeshBasicMaterial({
       color: 0x488384,
       wireframe: true,
-      fog: true,
     });
     this.floor = new THREE.Mesh(this.planeGeo, this.planeMaterial);
 
-    this.cubeGeo = new THREE.BoxGeometry(1, 1, 1);
-    this.cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-    this.cube = new THREE.Mesh(this.cubeGeo, this.cubeMaterial);
+    // this.cubeGeo = new THREE.PlaneBufferGeometry(30, 30, 200, 200);
+    // this.cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    // this.cube = new THREE.Mesh(this.cubeGeo, this.cubeMaterial);
 
+    this.floor.rotation.x = 5;
     this.scene.add(this.floor);
     this.scene.add(this.cube);
-    // this.camera.position.x = 5;
-    // this.camera.position.y = 0;
     this.camera.position.z = 2;
   }
 
   componentDidMount() {
-    this.state.sceneItems.forEach(item => {
-      this.scene.add(item);
-    });
-
     document.getElementById('canvas').appendChild(this.renderer.domElement);
     this.animate();
   }
 
   animate = () => {
     requestAnimationFrame(this.animate);
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    if (keyboard[87]) {
+      // W key
+      this.camera.position.x -= Math.sin(this.camera.rotation.y) * 0.5;
+      this.camera.position.z -= -Math.cos(this.camera.rotation.y) * 0.5;
+    }
+    if (keyboard[83]) {
+      // S key
+      this.camera.position.x += Math.sin(this.camera.rotation.y) * 0.5;
+      this.camera.position.z += -Math.cos(this.camera.rotation.y) * 0.5;
+    }
+    if (keyboard[68]) {
+      // D key
+      this.camera.position.x +=
+        Math.sin(this.camera.rotation.y + Math.PI / 2) * 0.5;
+      this.camera.position.z +=
+        -Math.cos(this.camera.rotation.y + Math.PI / 2) * 0.5;
+    }
+    if (keyboard[65]) {
+      // A key
+      this.camera.position.x +=
+        Math.sin(this.camera.rotation.y - Math.PI / 2) * 0.5;
+      this.camera.position.z +=
+        -Math.cos(this.camera.rotation.y - Math.PI / 2) * 0.5;
+    }
+    if (keyboard[37]) { //left arrow
+      this.camera.rotation.y -= 0.01;
+    }
+    if (keyboard[39]) { //right arrow
+      this.camera.rotation.y += 0.01;
+    }
+
+    // this.floor.rotation.x += 0.01;
+    // this.floor.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
   };
 
