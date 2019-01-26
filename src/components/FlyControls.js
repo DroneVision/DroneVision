1a57555bef0d12c03963b6b497c7f5c3f0bfe68e
Josh Feinstein <pillboxmediainc@gmail.com>
2019-01-25 19:11:33 -0500

    adds new fly screen for real time flight control

--- src/components/FlyControls.js
+++ src/components/FlyControls.js
@@ -1,65 +1,76 @@
-import React from 'react'
+import React from 'react';
 import { Button } from 'semantic-ui-react';
 
 const FlyControls = props => {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>
-            <p>FLY IN REAL TIME</p>
-            <Button onClick={() => props.realTimeTakeOff()}>Take Off</Button>
-            <br />
-            <br />
-            <Button onClick={() => props.realTimeFly(`up ${props.distance}`)}>
-              Up
-            </Button>
-            <Button
-              onClick={() => props.realTimeFly(`forward ${props.distance}`)}
-            >
-              Forward
-            </Button>
-            <br />
-            <Button
-              onClick={() => props.realTimeFly(`right ${props.distance}`)}
-            >
-              Right
-            </Button>
-            <Button onClick={() => props.realTimeFly(`left ${props.distance}`)}>
-              Left
-            </Button>
-            <br />
-            <Button onClick={() => props.realTimeFly(`down ${props.distance}`)}>
-              Down
-            </Button>
-            <Button onClick={() => props.realTimeFly(`back ${props.distance}`)}>
-              Back
-            </Button>
-            <br />
-            <br />
-            <Button onClick={() => props.realTimeFly(`flip f`)}>
-              Front-Flip
-            </Button>
-            <Button onClick={() => props.realTimeFly(`flip b`)}>
-              Back-Flip
-            </Button>
-            <Button onClick={() => props.realTimeFly(`flip l`)}>
-              Left-Flip
-            </Button>
-            <Button onClick={() => props.realTimeFly(`flip r`)}>
-              Right-Flip
-            </Button>
-            <br />
-            <br />
-            <Button onClick={() => props.realTimeFly(`land`)}>Land</Button>
-            &nbsp;&nbsp;&nbsp;
-            <Button onClick={() => props.realTimeFly(`emergency`)}>
-              EMERGENCY STOP
-            </Button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div id="fly-controls">
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <Button onClick={() => this.realTimeTakeOff()}>Take Off</Button>
+              <br />
+              <br />
+              <Button
+                onClick={() => this.realTimeFly(`up ${this.props.distance}`)}
+              >
+                Up
+              </Button>
+              <Button
+                onClick={() =>
+                  this.realTimeFly(`forward ${this.props.distance}`)
+                }
+              >
+                Forward
+              </Button>
+              <br />
+              <Button
+                onClick={() => this.realTimeFly(`right ${this.props.distance}`)}
+              >
+                Right
+              </Button>
+              <Button
+                onClick={() => this.realTimeFly(`left ${this.props.distance}`)}
+              >
+                Left
+              </Button>
+              <br />
+              <Button
+                onClick={() => this.realTimeFly(`down ${this.props.distance}`)}
+              >
+                Down
+              </Button>
+              <Button
+                onClick={() => this.realTimeFly(`back ${this.props.distance}`)}
+              >
+                Back
+              </Button>
+              <br />
+              <br />
+              <Button onClick={() => this.realTimeFly(`flip f`)}>
+                Front-Flip
+              </Button>
+              <Button onClick={() => this.realTimeFly(`flip b`)}>
+                Back-Flip
+              </Button>
+              <Button onClick={() => this.realTimeFly(`flip l`)}>
+                Left-Flip
+              </Button>
+              <Button onClick={() => this.realTimeFly(`flip r`)}>
+                Right-Flip
+              </Button>
+              <br />
+              <br />
+              <Button onClick={() => this.realTimeFly(`land`)}>Land</Button>
+              &nbsp;&nbsp;&nbsp;
+              <Button onClick={() => this.realTimeFly(`emergency`)}>
+                EMERGENCY STOP
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
