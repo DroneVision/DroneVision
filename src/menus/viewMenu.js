9d5c6a10710be8706578cda184b477753e301df8
topkoong <engineer_top@msn.com>
2019-02-02 11:57:03 -0500

    Refactor view menu item code closes-#27

--- src/menus/viewMenu.js
+++ src/menus/viewMenu.js
@@ -2,76 +2,36 @@ module.exports = BrowserWindow => {
 
     // Create a view menu on the menu bar called "View"
     // This view menu supports OSX and other OSs
-    
-    let viewMenu;
-    if(process.platform === 'darwin') {
-        const viewMenuOSX = {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Dev tools',
-                    accelerator: 'CommandOrControl+Option+I',
-                    click() {
-                        BrowserWindow.webContents.openDevTools()
-                    }
-                },
-                {
-                    label: 'Refresh',
-                    accelerator: 'CmdOrCtrl+R',
-                    click() {
-                        BrowserWindow.reload()
-                    }
-                },
-                {
-                    label: 'Zoom In',
-                    accelerator: 'CmdOrCtrl+Plus',
-                    role: 'zoomin'
-                },
-                {
-                    label: 'Zoom Out',
-                    accelerator: 'CmdOrCtrl+-',
-                    role: 'zoomout'
-                },
-                
-                { role: 'togglefullscreen' }
-            ]
-        };
-    
-        viewMenu = viewMenuOSX;
-    } else {
-        const viewMenuOtherOS = {
-            label: 'View',
-            submenu: [
-                {
-                    label: 'Dev tools',
-                    accelerator: 'CommandOrControl+Shift+I',
-                    click() {
-                        BrowserWindow.webContents.openDevTools()
-                    }
-                },
-                {
-                    label: 'Refresh',
-                    accelerator: 'CmdOrCtrl+R',
-                    click() {
-                        BrowserWindow.reload()
-                    }
-                },
-                {
-                    label: 'Zoom In',
-                    accelerator: 'CmdOrCtrl+Plus',
-                    role: 'zoomin'
-                },
-                {
-                    label: 'Zoom Out',
-                    accelerator: 'CmdOrCtrl+-',
-                    role: 'zoomout'
-                },
-                
-                { role: 'togglefullscreen' }
-            ]
-        };
-    
-        viewMenu = viewMenuOtherOS;
-    }
+    const viewMenu = {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Dev tools',
+                accelerator: process.platform === 'darwin' ? 'CommandOrControl+Option+I' : 'CommandOrControl+Shift+I',
+                click() {
+                    BrowserWindow.webContents.openDevTools()
+                }
+            },
+            {
+                label: 'Refresh',
+                accelerator: 'CmdOrCtrl+R',
+                click() {
+                    BrowserWindow.reload()
+                }
+            },
+            {
+                label: 'Zoom In',
+                accelerator: 'CmdOrCtrl+Plus',
+                role: 'zoomin'
+            },
+            {
+                label: 'Zoom Out',
+                accelerator: 'CmdOrCtrl+-',
+                role: 'zoomout'
+            },
+
+            { role: 'togglefullscreen' }
+        ]
+    };
     return viewMenu;
 }
