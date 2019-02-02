f05b9131c3c755a14b5ebf7e44f628d5a60677ff
topkoong <engineer_top@msn.com>
2019-02-02 15:39:41 -0500

    Remove unnecessary comments in droneMenu.js

--- src/menus/droneMenu.js
+++ src/menus/droneMenu.js
@@ -47,74 +47,69 @@ module.exports = mainWindow => {
         let wifiNetworks
         try {
             wifiNetworks = await wifi.scan();
-            console.log('wifiNetworks: ', wifiNetworks)
-
             // If we can get all wifi network
+            if (wifiNetworks.length > 0) {
+                // We look for a drone wifi network
+                const droneWifiNetworks = wifiNetworks.filter(network => {
+                    return network.ssid.startsWith('TELLO-')
+                }).map(droneNetwork => droneNetwork.ssid)
 
-        if (wifiNetworks.length > 0) {
-            // We look for a drone wifi network
-            const droneWifiNetworks = wifiNetworks.filter(network => {
-                return network.ssid.startsWith('TELLO-')
-            }).map(droneNetwork => droneNetwork.ssid)
-
-            console.log('droneWifiNetworks: ', droneWifiNetworks)
-            if (droneWifiNetworks.length > 0) {
-                // Select drone network
-                let selectedDroneSSID;
-                // Display drone networks to connect to
-                const buttons = [...droneWifiNetworks, 'Cancel']
-                console.log(buttons);
-                const res = dialog.showMessageBox(mainWindow, {
-                    type: 'info',
-                    buttons: buttons,
-                    message: 'Select the Drone network',
-                    detail: 'Select the Drone network you would like to connect to'
-                })
-                // Set the drone SSID if the user doesn't click the cancel button.
-                if(res !== buttons.length - 1) {
-                    selectedDroneSSID = buttons[res]
-                    // Create a Drone accesspoint object
-                    const droneAP = {
-                        ssid: selectedDroneSSID,
-                        password: ''
-                    };
-    
-                    if (connectButtonCounter < 1) {
-                        // Connect to a drone wifi network
-                        wifi.connect(droneAP, (err) => {
-                            if (err) {
-                                console.error(err);
-                                // console.log(`Couldn't connect to the Drone Wifi Network`);
-                            }
-                            connectButtonCounter++;
-                            droneMenu.submenu[0].enabled = false
+                console.log('droneWifiNetworks: ', droneWifiNetworks)
+                if (droneWifiNetworks.length > 0) {
+                    // Select drone network
+                    let selectedDroneSSID;
+                    // Display drone networks to connect to
+                    const buttons = [...droneWifiNetworks, 'Cancel']
+                    console.log(buttons);
+                    const res = dialog.showMessageBox(mainWindow, {
+                        type: 'info',
+                        buttons: buttons,
+                        message: 'Select the Drone network',
+                        detail: 'Select the Drone network you would like to connect to'
+                    })
+                    // Set the drone SSID if the user doesn't click the cancel button.
+                    if(res !== buttons.length - 1) {
+                        selectedDroneSSID = buttons[res]
+                        // Create a Drone accesspoint object
+                        const droneAP = {
+                            ssid: selectedDroneSSID,
+                            password: ''
+                        };
+        
+                        if (connectButtonCounter < 1) {
+                            // Connect to a drone wifi network
+                            wifi.connect(droneAP, (err) => {
+                                if (err) {
+                                    console.error(err);
+                                    // console.log(`Couldn't connect to the Drone Wifi Network`);
+                                }
+                                connectButtonCounter++;
+                                droneMenu.submenu[0].enabled = false
+                                dialog.showMessageBox(mainWindow, {
+                                    type: 'info',
+                                    buttons: ['OK'],
+                                    message: 'Sucessfully connected',
+                                    detail: `Connected to the Drone Wifi: ${droneAP.ssid}`
+                                })
+                            });
+                        } else {
                             dialog.showMessageBox(mainWindow, {
-                                type: 'info',
+                                type: 'warning',
                                 buttons: ['Cancel'],
-                                message: 'Sucessfully connected',
-                                detail: `Connected to the Drone Wifi: ${droneAP.ssid}`
+                                message: 'Already connected',
+                                detail: `You have already connected to the Drone Wifi: ${droneAP.ssid}`
                             })
-                        });
+                        }
                     } else {
-                        dialog.showMessageBox(mainWindow, {
-                            type: 'warning',
-                            buttons: ['Cancel'],
-                            message: 'Already connected',
-                            detail: `You have already connected to the Drone Wifi: ${droneAP.ssid}`
-                        })
+                        return;
                     }
-                } else {
-                    return;
                 }
+                // For Top
+                // console.log(`Couldn't find the drone WiFi network`);
             }
-            // For Top
-            // console.log(`Couldn't find the drone WiFi network`);
-        }
-
         } catch (error) {
             console.error(error);
         }
-
     }
 
     // Disconnect from current network
