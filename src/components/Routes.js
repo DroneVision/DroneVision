40ab7955228aa11f7dcc308400db23bbd6037890
Justin <mintzer.justin@gmail.com>
2019-02-02 22:44:13 -0500

    Added videos to navbar

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -6,6 +6,7 @@ import FlyScreen from '../screens/FlyScreen';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Sandbox from './Sandbox';
+import Videos from './Videos';
 
 const Routes = props => (
   <div>
@@ -15,6 +16,7 @@ const Routes = props => (
       <Route path="/build" component={Build} />
       <Route path="/run" component={Run} />
       <Route path="/fly" component={FlyScreen} />
+      <Route path="/videos" component={Videos}/>
       <Route path="/sandbox" component={Sandbox} />
     </Switch>
     <Footer />
