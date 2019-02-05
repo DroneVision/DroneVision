73292005e1ef21112eb2c24a5660411de81f0d1d
Justin <mintzer.justin@gmail.com>
2019-02-05 15:26:52 -0500

    Removed videos from codebase and moved video library outisde of src

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -5,7 +5,7 @@ class Video extends Component {
     const {video} = this.props
     let url = null;
     if (video) {
-      url = require(`../videos/${this.props.video}`)
+      url = require(`../../videos/${this.props.video}`)
       console.log(url)
     }
     return (
